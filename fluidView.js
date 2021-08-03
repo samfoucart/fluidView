a837d95f246ca37fc12b579c2f5bd6daabd4c8cf
Sam Foucart <samfoucart25@gmail.com>
2021-08-03 17:47:17 -0400

    Added all rendering into the fluidView class

--- fluidView.js
+++ fluidView.js
@@ -1,17 +1,56 @@
 class FluidView {
-    constructor(size, dt, diffusion, viscosity) {
+    constructor(size, dt, diffusion, viscosity, ctx, cellScale) {
         this.N = size;
         this.dt = dt;
         this.diffusion = diffusion;
         this.viscosity = viscosity;
 
-        this.s = new Array(N * N);
-        this.density = new Array(N * N);
+        this.s = new Array(this.N * this.N);
+        this.density = new Array(this.N * this.N);
 
-        this.Vx = new Array(N * N);
-        this.Vy = new Array(N * N);
+        this.Vx = new Array(this.N * this.N);
+        this.Vy = new Array(this.N * this.N);
 
-        this.Vx0 = new Array(N * N);
-        this.Vy0 = new Array(N * N);
+        this.Vx0 = new Array(this.N * this.N);
+        this.Vy0 = new Array(this.N * this.N);
+
+        this.ctx = ctx;
+        this.cellScale = cellScale;
+
+        this.activeY = -1;
+        this.activeX = -1;
     }
+
+    render() {
+        this.ctx.fillStyle = "white";
+        this.ctx.fillRect(0, 0, this.N * this.cellScale, this.N * this.cellScale);
+        for (var col = 0; col < this.N * this.cellScale; col += this.cellScale) {
+            for (var row = 0; row < this.N * this.cellScale; row += this.cellScale) {
+                if (row == this.activeY && col == this.activeX) {
+                    this.ctx.fillStyle = "black";
+                    this.ctx.fillRect(col, row, this.cellScale, this.cellScale);
+                } else {
+                    let brightness = FluidView.lerp(0, 255, this.density[this.index(row, col)]);
+                    this.ctx.fillStyle = 'rgb(' + brightness + ', ' + brightness + ', ' + brightness + ')';
+                    this.ctx.strokeRect(col, row, this.cellScale, this.cellScale);
+                }
+            }
+        }
+    }
+
+    hover(x, y) {
+        this.activeY = Math.floor(y / this.cellScale) * this.cellScale;
+        this.activeX = Math.floor(x / this.cellScale) * this.cellScale;
+        console.log(this.activeX);
+        console.log(this.activeY);
+    }
+
+    static lerp(a, b, alpha) {
+        return a + (alpha * (b - a));
+    }
+
+    index(row, column) {
+        return column + (row * this.N);
+    }
+
 };
--- index.js
+++ index.js
@@ -4,32 +4,35 @@ webGLCanvas.width = 1024;
 webGLCanvas.height = 1024;
 let cellScale = 8;
 let numCells = 128;
-webGLContext.fillStyle = "blue";
-webGLContext.fillRect(0, 0, webGLCanvas.width, webGLCanvas.height);
-drawgrid(webGLContext);
+// webGLContext.fillStyle = "blue";
+// webGLContext.fillRect(0, 0, webGLCanvas.width, webGLCanvas.height);
+// drawgrid(webGLContext);
 
-function drawgrid(ctx2d) {
-    for (var col = 0; col < webGLCanvas.width; col += cellScale) {
-        for (var row = 0; row < webGLCanvas.height; row += cellScale) {
-            ctx2d.strokeRect(col, row, cellScale, cellScale);
-        }
-    }
-}
+// function drawgrid(ctx2d) {
+//     for (var col = 0; col < webGLCanvas.width; col += cellScale) {
+//         for (var row = 0; row < webGLCanvas.height; row += cellScale) {
+//             ctx2d.strokeRect(col, row, cellScale, cellScale);
+//         }
+//     }
+// }
 
-function index(row, column) {
-    return column + (row * numCells);
-}
+// function index(row, column) {
+//     return column + (row * numCells);
+// }
 
-webGLCanvas.addEventListener('mousemove', e => {
-    webGLContext.fillStyle = "red";
-    let row = Math.floor(e.offsetY / cellScale) * cellScale;
-    let col = Math.floor(e.offsetX / cellScale) * cellScale;
-    webGLContext.fillRect(col, row, cellScale, cellScale);
-});
+
 
 function main() {
-    let fluidView = new FluidView(128, .001, 1, 1);
+    let fluidView = new FluidView(128, .001, 1, 1, webGLContext, cellScale);
+    fluidView.render();
+
+    webGLCanvas.addEventListener('mousemove', e => {
+        fluidView.hover(e.offsetX, e.offsetY);
+        fluidView.render();
+    });
 }
+
+main();
 
 // webGLCanvas.onmouseover = e => {
 //     ctx2d.strokeRect(col.offset, row, 8, 8);
