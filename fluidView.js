4211ed5190d6f7bb3abc482fa4f3ea253dcc6887
Sam Foucart <samfoucart25@gmail.com>
2021-08-03 23:09:49 -0400

    It almost works. It needs scanning for a math bug

--- fluidView.js
+++ fluidView.js
@@ -78,9 +78,18 @@ class FluidView {
     updateFluid() {
         // TRY SWITCHING THE Vx0 AND Vx AND density AND s TO SEE WHAT HAPPENS
         // I THINK THE AUTHOR ACCIDENTALLY WROTE IT BACKWARDS
-        this.diffuse(1, this.Vx, this.Vx0, this.viscosity, this.dt, 4);
-        this.diffuse(2, this.Vy, this.Vy0, this.viscosity, this.dt, 4);
-        this.diffuse(0, this.density, this.s, this.diffusion, this.dt, this.iterations);
+        this.diffuse(1, this.Vx0, this.Vx, this.viscosity, this.dt, this.iterations);
+        this.diffuse(2, this.Vy0, this.Vy, this.viscosity, this.dt, this.iterations);
+
+        this.project(this.Vx0, this.Vy0, this.Vx, this.Vy);
+
+        this.advect(1, this.Vx, this.Vx0, this.Vx0, this.Vy0, this.dt);
+        this.advect(2, this.Vy, this.Vy0, this.Vx0, this.Vy0, this.dt);
+
+        this.project(this.Vx0, this.Vy0, this.Vx, this.Vy);
+
+        this.diffuse(0, this.s, this.density, this.diffusion, this.dt, this.iterations);
+        this.advect(0, this.density, this.s, this.Vx, this.Vy, this.dt)
     }
 
     diffuse (b, x, x0, diffusion, dt, iter) {
@@ -98,6 +107,82 @@ class FluidView {
             }
             //setbnd
         }
+    }
+
+    advect(b, d, d0, u, v, dt) {
+        let dt0 = dt*this.N;
+        for (var i = 1; i < this.N - 1; ++i ) {
+            for (var j = 1; j < this.N - 1; ++j) {
+                let x = i - (dt0 * u[this.index(i, j)]);
+                let y = j - (dt0 * u[this.index(i, j)]);
+
+                if (x < 0.5) {
+                    x = 0.5;
+                }
+                if (x > this.N + .5) {
+                    x = this.N + .5;
+                }
+                let i0 = Math.floor(x);
+                let i1 = i0 + 1;
+    
+                if (y < .5) {
+                    y = .5;
+                }
+                if (y > this.N + .5) {
+                    y = this.N + .5;
+                }
+                let j0 = Math.floor(y);
+                let j1 = j0 + 1;
+    
+                let s1 = x - i0;
+                let s0 = 1 - s1;
+                let t1 = y - j0;
+                let t0 = 1 - t1;
+    
+                d[this.index(i, j)] = s0 * (t0 * d0[this.index(i0, j0)]  +
+                                            t1 * d0[this.index(i0, j1)]) +
+                                      s1 * (t0 * d0[this.index(i1, j0)]  +
+                                            t1 * d0[this.index(i1, j1)]);    
+            }
+        }
+        //set_bnd
+    }
+
+    project(u, v, p, div) {
+        let h = 1.0 / this.N;
+        for (var i = 1; i < this.N - 1; ++i) {
+            for (var j = 1; j < this.N - 1; ++j) {
+                div[this.index(i, j)] = -.5 * h * (u[this.index(i + 1, j)] - 
+                                                    u[this.index(i - 1, j)] + 
+                                                    v[this.index(i, j + 1)] - 
+                                                    v[this.index(i, j - 1)]);
+
+                p[this.index(i, j)] = 0;
+            }
+        }
+        //setbnd
+
+        for (var k = 0; k < this.iter; ++k) {
+            for (var i = 1; i < this.N - 1; ++i) {
+                for (var j = 1; j < this.N - 1; ++j) {
+                    p[this.index(i, j)] = (div[this.index(i, j)] + (
+                                p[this.index(i-1, j)] +
+                                p[this.index(i+1, j)] + 
+                                p[this.index(i, j-1)] + 
+                                p[this.index(i, j+1)]) / 4);
+                }
+            }
+            //setbnd
+        }
+
+        for (var i = 1; i < this.N - 1; ++i) {
+            for (var j = 1; j < this.N - 1; ++j) {
+                u[this.index(i, j)] = u[this.index(i, j)] - (.5 * (p[this.index(i+1, j)] - p[this.index(i-1, j)]) / h);
+                u[this.index(i, j)] = u[this.index(i, j)] - (.5 * (p[this.index(i, j+1)] - p[this.index(i, j-1)]) / h);
+            }
+        }
+
+        //setbnd
     }
 
 };
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let numCells = 64;
 
 
 function main() {
-    let fluidView = new FluidView(numCells, .001, 1000, 1000, webGLContext, webGLCanvas, cellScale);
+    let fluidView = new FluidView(numCells, .001, .01, .1, webGLContext, webGLCanvas, cellScale);
     fluidView.render();
 
     webGLCanvas.addEventListener('mousemove', e => {
@@ -15,7 +15,7 @@ function main() {
     });
 
     webGLCanvas.addEventListener('mousedown', e => {
-        fluidView.addDensity(e.offsetX, e.offsetY, 1);
+        fluidView.addDensity(e.offsetX, e.offsetY, .1);
         //fluidView.addDensity(e.offsetX - (fluidView.cellScale), e.offsetY, 1);
         //fluidView.addDensity(e.offsetX + (fluidView.cellScale), e.offsetY, 1);
         //fluidView.addDensity(e.offsetX, e.offsetY + (fluidView.cellScale), 1);
